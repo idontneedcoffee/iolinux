1c1f3ebcf56e72b6220f49ecb6cc00ef9de1df50
idontneedcoffee <idontneedcoffee@idnc.sk>
2012-11-27 02:48:04 +0100

    1

---
+++ my.io.server/Roles/ClientDock/ignition.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+source ./functions.sh
+
+# check device
+
+# check device action
+
+# remove > client_undock.sh
+
+# add > client_dock.sh
+
+# >>>>
+
+# mount device
+
+# check my.io image
+
+# decrypt && loop mount
+
+# check user
+
+# test client(db,ui paths/binx)
+
+# start client [db(mongodb-lxc-socket) && ui(node-webkit-lxc-socket)]
+
+
+
+
